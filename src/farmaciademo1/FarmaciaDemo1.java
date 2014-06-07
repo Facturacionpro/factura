6868ed2a7a0d5e1bb887a65dbee2f014accebfa6
murillomauricio <mauriciomurillocastilla@gmail.com>
2014-06-07 11:36:20 -0600

    Agregando nuevo proyecto de Farmacia

---
+++ src/farmaciademo1/FarmaciaDemo1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package farmaciademo1;
+
+/**
+ *
+ * @author MAURICIO
+ */
+public class FarmaciaDemo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
